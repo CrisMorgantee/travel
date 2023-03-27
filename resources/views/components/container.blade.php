fbe2473746fcbd34dd4b683a4dc02b3c2c94f469
Cristiano Morgante <cristianomorgante@hotmail.com>
2023-03-27 15:56:00 -0300

    update readme and make container component

--- resources/views/components/container.blade.php
+++ resources/views/components/container.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes}} class="mt-0 mx-auto h-full w-full max-w-5xl bg-slate-700">
-  {{ $slot }}
-</div>
+  <div {{ $attributes->class([ "mt-0 mx-auto h-full w-full max-w-5xl" ]) }}>
+    {{ $slot }}
+  </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,13 @@
 <x-layouts.base>
-  <x-container>
-    <h1>Hello Blade!!!</h1>
+  <x-container class="flex items-center justify-center">
+    <section class="font-semibold flex flex-col h-full justify-center">
+      <h1 class="font-bold text-3xl mb-8">Boilerplate Laravel</h1>
+
+      <h3 class="text-lg flex items-center">
+        <span class="mr-3">Laravel Version</span>
+        <span class="bg-slate-600 text-xs p-1 rounded"> {{ app()->version() }} </span>
+      </h3>
+
+    </section>
   </x-container>
 </x-layouts.base>
